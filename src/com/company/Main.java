b86a86da1aa6ae9075b05a936a184a0f7b3d4e80
phamthinh14 <phamthinh1994@gmail.com>
2019-06-17 14:27:38 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int [] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for (int i = 9; i > -1; i--) {
+            System.out.print(numbers[i] + ", ");
+        }
+    }
+}
